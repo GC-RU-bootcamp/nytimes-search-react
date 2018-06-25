51f30c10ad4d02cec1544fc98b4badd66c71139f
Cender <CenderG@win.dowjones.net>
2018-06-25 00:06:36 -0400

    initial upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 var bodyParser = require("body-parser");
 const routes = require("./routes");
-const PORT = process.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 
